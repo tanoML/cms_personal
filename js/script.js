f02805cfb1a92b7d4e987133e36e60676d00e51d
tanoLM <tanokylm@hotmail.com>
2020-12-11 17:08:19 -0600

    Edicion del script para los inputs

--- js/script.js
+++ js/script.js
@@ -1,11 +1,133 @@
 $("#bas").on('change', function() {
-
     if ($(this).is(':checked')) {
-        //alert("su we");
         $("#bas_sa").prop("disabled", false);
+        $("#bas_pa").prop("disabled", false);
     } else {
         $("#bas_sa").val('');
         $("#bas_sa").prop("disabled", true);
+        $("#bas_pa").val('');
+        $("#bas_pa").prop("disabled", true);
     }
+});
 
+$("#tiba").on('change', function() {
+    if ($(this).is(':checked')) {
+        $("#tiba_sa").prop("disabled", false);
+        $("#tiba_pa").prop("disabled", false);
+    } else {
+        $("#tiba_sa").val('');
+        $("#tiba_sa").prop("disabled", true);
+        $("#tiba_pa").val('');
+        $("#tiba_pa").prop("disabled", true);
+    }
+});
+$("#cii").on('change', function() {
+    if ($(this).is(':checked')) {
+        $("#cii_sa").prop("disabled", false);
+        $("#cii_pa").prop("disabled", false);
+    } else {
+        $("#cii_sa").val('');
+        $("#cii_sa").prop("disabled", true);
+        $("#cii_pa").val('');
+        $("#cii_pa").prop("disabled", true);
+    }
+});
+$("#bcat").on('change', function() {
+    if ($(this).is(':checked')) {
+        $("#bcat_sa").prop("disabled", false);
+        $("#bcat_pa").prop("disabled", false);
+    } else {
+        $("#bcat_sa").val('');
+        $("#bcat_sa").prop("disabled", true);
+        $("#bcat_pa").val('');
+        $("#bcat_pa").prop("disabled", true);
+    }
+});
+$("#cfa").on('change', function() {
+    if ($(this).is(':checked')) {
+        $("#cfa_sa").prop("disabled", false);
+        $("#cfa_pa").prop("disabled", false);
+    } else {
+        $("#cfa_sa").val('');
+        $("#cfa_sa").prop("disabled", true);
+        $("#cfa_pa").val('');
+        $("#cfa_pa").prop("disabled", true);
+    }
+});
+$("#ge").on('change', function() {
+    if ($(this).is(':checked')) {
+        $("#ge_sa").prop("disabled", false);
+        $("#ge_pa").prop("disabled", false);
+    } else {
+        $("#ge_sa").val('');
+        $("#ge_sa").prop("disabled", true);
+        $("#ge_pa").val('');
+        $("#ge_pa").prop("disabled", true);
+    }
+});
+$("#apt").on('change', function() {
+    if ($(this).is(':checked')) {
+        $("#apt_sa").prop("disabled", false);
+        $("#apt_pa").prop("disabled", false);
+    } else {
+        $("#apt_sa").val('');
+        $("#apt_sa").prop("disabled", true);
+        $("#apt_pa").val('');
+        $("#apt_pa").prop("disabled", true);
+    }
+});
+$("#egt").on('change', function() {
+    if ($(this).is(':checked')) {
+        $("#egt_sa").prop("disabled", false);
+        $("#egt_pa").prop("disabled", false);
+    } else {
+        $("#egt_sa").val('');
+        $("#egt_sa").prop("disabled", true);
+        $("#egt_pa").val('');
+        $("#egt_pa").prop("disabled", true);
+    }
+});
+$("#crt").on('change', function() {
+    if ($(this).is(':checked')) {
+        $("#crt_sa").prop("disabled", false);
+        $("#crt_pa").prop("disabled", false);
+    } else {
+        $("#crt_sa").val('');
+        $("#crt_sa").prop("disabled", true);
+        $("#crt_pa").val('');
+        $("#crt_pa").prop("disabled", true);
+    }
+});
+$("#pht").on('change', function() {
+    if ($(this).is(':checked')) {
+        $("#pht_sa").prop("disabled", false);
+        $("#pht_pa").prop("disabled", false);
+    } else {
+        $("#pht_sa").val('');
+        $("#pht_sa").prop("disabled", true);
+        $("#pht_pa").val('');
+        $("#pht_pa").prop("disabled", true);
+    }
+});
+$("#bit").on('change', function() {
+    if ($(this).is(':checked')) {
+        $("#bit_sa").prop("disabled", false);
+        $("#bit_pa").prop("disabled", false);
+    } else {
+        $("#bit_sa").val('');
+        $("#bit_sa").prop("disabled", true);
+        $("#bit_pa").val('');
+        $("#bit_pa").prop("disabled", true);
+    }
+});
+$("#api").on('change', function() {
+    if ($(this).is(':checked')) {
+        $("#api_sa").prop("disabled", false);
+        $("#api_pa").prop("disabled", false);
+    } else {
+        $("#api_sa").val('');
+        $("#api_sa").prop("disabled", true);
+        $("#api_pa").val('');
+        $("#api_pa").prop("disabled", true);
+    }
 });
